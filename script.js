32af819122797eebd1fa23a4040368c8c744f1a2
Matt Gohr <gohr89@gmail.com>
2018-01-14 22:28:56 -0500

    Finshed HTML Layout Basic

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-// $(document).on("click", "#my-button", callFunctino);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAIWrc7C-g6Zcd1P7yCIOBLjktV7ATU6q8",
+  authDomain: "first-project-3fa4f.firebaseapp.com",
+  databaseURL: "https://first-project-3fa4f.firebaseio.com",
+  projectId: "first-project-3fa4f",
+  storageBucket: "first-project-3fa4f.appspot.com",
+  messagingSenderId: "128232668232"
+};
+firebase.initializeApp(config);
 
-alert('hey');
-console.log('there');
+var database = firebase.database();
+
+function mainClickEvent() {
+
+  $("#butn").on('click', function(event) {
+    event.preventDefault();
+  });
+}
+
+
+
+
+
+$(document).on("click", ".btn", mainClickEvent);
