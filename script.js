de81a9e24cf237ca46fdc8ba0a9c63eec6ac71b9
Matt Gohr <gohr89@gmail.com>
2018-01-17 20:39:47 -0500

    Working on forEach loop for on Value for firebase

--- script.js
+++ script.js
@@ -12,40 +12,39 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 var player = 0;
-var player1, player2;
 
-function fillBlock(block, name, skip) {
+function fillBlock(block, name, skip, wins, losses) {
   //remove the text from the block
   $('#' + block).children().remove()
 
   //add elements
   var NameP = $('<p>');
   NameP.text(name);
-  var rock = $("<button class=\'btn btn-secondary\'> Rock </button>");
-  var paper = $("<button class=\'btn btn-secondary\'> Paper </button>");
-  var scissors = $("<button class=\'btn btn-secondary\'> Scissors </button>");
-  var p = $('<p class=\'name\'>' + name + '</p>');
-  var wins = $('<p class=\'wins\'>Wins: </p>');
-  var losses = $('<p class=\'losses\'>Losses: </p>');
+  var rock = $("<div class='btn-holder'><button class=\'btn btn-secondary\'> Rock </button></div>");
+  var paper = $("<div class='btn-holder'><button class=\'btn btn-secondary\'> Paper </button></div>");
+  var scissors = $("<div class='btn-holder'><button class=\'btn btn-secondary\'> Scissors </button></div>");
+  var nameP = $('<p class=\'name\'>' + name + '</p>');
+  var winsP = $('<p class=\'wins\'>Wins: </p>');
+  var lossesP = $('<p class=\'losses\'>Losses: </p>');
 
   //create span tags
   var spanWin = $("<span>");
   var spanLos = $("<span>");
 
   //add the inital score
-  spanWin.text('0');
-  spanLos.text('0');
+  spanWin.text(wins);
+  spanLos.text(losses);
 
   //span ids as the players name so it's easier to replace later on in the game
   spanWin.attr('id', 'wins-' + name);
   spanLos.attr('id', 'losses-' + name);
 
   //append span to wins and losses
-  wins.append(spanWin);
-  losses.append(spanLos);
+  winsP.append(spanWin);
+  lossesP.append(spanLos);
 
   // add all the stuff to the block
-  $('#' + block).append(p);
+  $('#' + block).append(nameP);
 
   //skip the adding of rock
   if (skip === true) {
@@ -55,8 +54,8 @@ function fillBlock(block, name, skip) {
     $('#' + block).append(paper);
     $('#' + block).append(scissors);
   }
-  $('#' + block).append(wins);
-  $('#' + block).append(losses);
+  $('#' + block).append(winsP);
+  $('#' + block).append(lossesP);
 }
 
 function mainClickEvent() {
@@ -99,11 +98,12 @@ function mainClickEvent() {
   });
 }
 
-
+//on load fill Blocks
 database.ref("players").on("value", function(dataSnapshot) {
 
   dataSnapshot.forEach(function(data) {
     console.log("The Key is" + data.key + "and name is " + data.val().name);
+    player++
   });
 
 });
