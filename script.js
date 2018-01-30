cfa38caaf652d3e16152fe3852bec04c5a0c3c78
Matt Gohr <gohr89@gmail.com>
2018-01-29 22:11:53 -0500

    fixed wins & losses

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/*
+
+SORRY FOR THE MESSY CODE
+
+*/
+
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyAIWrc7C-g6Zcd1P7yCIOBLjktV7ATU6q8",
@@ -30,28 +37,28 @@ function fillBlock(playerNum, name, skip, wins, losses) {
   //init elements
   var NameP = $('<p>');
   NameP.text(name);
-  var rock = $("<div class='btn-holder'><button class=\'btn btn-secondary\'> Rock </button></div>");
-  var paper = $("<div class='btn-holder'><button class=\'btn btn-secondary\'> Paper </button></div>");
-  var scissors = $("<div class='btn-holder'><button class=\'btn btn-secondary\'> Scissors </button></div>");
-  var nameP = $('<p class=\'name\'>' + name + '</p>');
-  var winsP = $('<p class=\'wins\'>Wins: </p>');
-  var lossesP = $('<p class=\'losses\'>Losses: </p>');
+  var rock = $("<div class='btn-holder'><button class='btn btn-secondary btn-rps'> Rock </button></div>");
+  var paper = $("<div class='btn-holder'><button class='btn btn-secondary btn-rps'> Paper </button></div>");
+  var scissors = $("<div class='btn-holder'><button class='btn btn-secondary btn-rps'> Scissors </button></div>");
+  var nameP = $("<p class=name>" + name + "</p>");
+  var winsP = $("<p class='wins'>Wins: <span id=wins-" + playerCount + ">" + wins + "</span> Losses: <span id=losses-" + playerCount + "></span>" + losses + "</p>");
+  // var lossesP = $('<p class=\'losses\'>Losses: </p>');
 
   //create span tags
   var spanWin = $("<span>");
   var spanLos = $("<span>");
 
   //add the inital score
-  spanWin.text(wins);
-  spanLos.text(losses);
+  // spanWin.text(wins);
+  // spanLos.text(losses);
 
   //span ids as the players name so it's easier to replace later on in the game
-  spanWin.attr('id', 'wins-' + name);
-  spanLos.attr('id', 'losses-' + name);
+  // spanWin.attr('id', 'wins-' + name);
+  // spanLos.attr('id', 'losses-' + name);
 
   //append span to wins and losses
-  winsP.append(spanWin);
-  lossesP.append(spanLos);
+  // winsP.append(spanWin);
+  // lossesP.append(spanLos);
 
   // add all the stuff to the block
   $('#' + block).append(nameP);
@@ -65,20 +72,14 @@ function fillBlock(playerNum, name, skip, wins, losses) {
     $('#' + block).append(scissors);
   }
   $('#' + block).append(winsP);
-  $('#' + block).append(lossesP);
-}
-
-function determinPlayer(something) {
-
-
-  //if player then
+  // $('#' + block).append(lossesP);
 }
 
 function mainClickEvent() {
 
   $("#start").on('click', function() {
 
-    if (playerCount === 0) {
+    if (playerCount === 1) {
 
       //get player
       myPlayer = $("#gamer-tag").val().trim();
@@ -96,7 +97,7 @@ function mainClickEvent() {
       database.ref("players").update(newPlayer);
       removeInput(myPlayer)
 
-    } else if (playerCount === 1) {
+    } else if (playerCount === 2) {
       //get variable
       myPlayer = $("#gamer-tag").val().trim();
       console.log(myPlayer);
@@ -118,55 +119,62 @@ function mainClickEvent() {
       console.log('too many players');
     }
   });
-}
+  $("#start").on('click', function() {
 
-//remove input button
-function removeInput(name) {
+  })
+  }
 
-  //gamer remove gamer tag
-  $("#gamer-tag-div").children().remove();
+  //remove input button
+  function removeInput(name) {
 
-  var helloMsg = $("<p>");
-  var helloMsgSpan = $("<span>");
-  var turnMsg = $("<p>");
-  helloMsg.text('Hi ' + name + ' You are Player ' + playerCount);
-  helloMsgSpan.attr('id', 'span-name');
-  helloMsg.append(helloMsgSpan);
-  turnMsg.text('it\'s your turn!');
-  turnMsg.attr('id', 'turn-div');
-
-  $("#gamer-tag-div").append(helloMsg)
-  $("#gamer-tag-div").append(turnMsg)
-
-}
-
-console.log('before added');
-//on load fill Blocks
-var firstRun = database.ref("players").on("child_added", function(childSnapshot, prevChildKey) {
-
-  fillBlock(Number(childSnapshot.key), childSnapshot.val().name, false, childSnapshot.val().wins, childSnapshot.val().losses);
-  playerCount++;
-  //myPlayer;
-  console.log('PLayer count: ' + playerCount);
-
-  //if too many players the
-  if (playerCount === 2 && !myPlayer) {
     //gamer remove gamer tag
     $("#gamer-tag-div").children().remove();
 
     var helloMsg = $("<p>");
-    helloMsg.text('There are too many players right now please try again later :)');
-    $("#gamer-tag-div").append(helloMsg);
-    //remove the text from the block
-    $('#right-block').children().remove();
-    $('#left-block').children().remove();
+    var helloMsgSpan = $("<span>");
+    var turnMsg = $("<p>");
+    helloMsg.text('Hi ' + name + ' You are Player ' + playerCount);
+    helloMsgSpan.attr('id', 'span-name');
+    helloMsg.append(helloMsgSpan);
+    turnMsg.text('it\'s your turn!');
+    turnMsg.attr('id', 'turn-div');
+
+    $("#gamer-tag-div").append(helloMsg)
+    $("#gamer-tag-div").append(turnMsg)
+
   }
-});
 
-//turn off updating
-// database.ref("players").off('child_added');
+  //on load fill Blocks
+  var firstRun = database.ref("players").on("child_added", function(childSnapshot, prevChildKey) {
 
-console.log(playerCount);
-mainClickEvent();
+    fillBlock(Number(childSnapshot.key), childSnapshot.val().name, false, childSnapshot.val().wins, childSnapshot.val().losses);
+    playerCount++;
+    //myPlayer;
+    console.log('PLayer count: ' + playerCount);
 
-$(document).on("click", ".btn", mainClickEvent);
+    //if too many players then tell them to come back later
+    if (playerCount === 3 && !myPlayer) {
+      //gamer remove gamer tag
+      $("#gamer-tag-div").children().remove();
+
+      //imput message
+      var helloMsg = $("<p>");
+      helloMsg.text('There are too many players right now please try again later :)');
+      $("#gamer-tag-div").append(helloMsg);
+
+      //remove the text from the block
+      $('#right-block').children().remove();
+      $('#left-block').children().remove();
+
+    }else if (playerCount === 2 && !myPlayer) {
+      // $('#left-block').children('.btn-holder').remove();
+    }
+  });
+
+  //turn off updating
+  // database.ref("players").off('child_added');
+
+  console.log(playerCount);
+  mainClickEvent();
+
+  $(document).on("click", ".btn", mainClickEvent);
